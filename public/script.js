fbe86256d6ebebd27d39143c12d1c3ad12c5c647
FapaKslapa <maroccostefano@itis-molinari.eu>
2024-02-05 22:04:09 +0100

    Render con collapse V1

--- public/script.js
+++ public/script.js
@@ -4,16 +4,18 @@ const inviaUser = document.getElementById("inviaUser");
 const username = document.getElementById("username");
 let tempo = 0; // Inserisci il tempo in secondi
 let countdown;
-
+const modalQuizBody = document.getElementById("modalQuizBody");
+const modalQuizTitle = document.getElementById("modalQuizTitle");
 const template = `<li>
 %DOMANDA
-</li>`
+</li>`;
 inviaUser.onclick = () => {
   if (username.value) {
     modalName.hide();
     getQuestion().then((data) => {
       modalQuiz.show();
-      console.log(data.questions[0]);
+      modalQuizTitle.innerHTML = data.title;
+      modalQuizBody.innerHTML = renderQuestion(data.questions);
       tempo = data.timer;
       countdown = setInterval(() => {
         if (tempo > 10) {
@@ -47,5 +49,40 @@ const getQuestion = () => {
   });
 };
 
-const renderQuestion = (array) => {
-};
+function renderQuestion(question) {
+  let html = ``;
+
+  question.forEach((answer, index) => {
+    html += `
+    <li>
+      <div class="row">
+        <div class="col">%TITOLO</div>
+        <div class="col">
+        <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+  <label class="form-check-label" for="inlineRadio1"><button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+  Toggle width collapse
+</button></label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+  <label class="form-check-label" for="inlineRadio2">2</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+  <label class="form-check-label" for="inlineRadio3">3 (disabled)</label>
+</div>
+        </div>
+      </div>
+      <div style="min-height: 120px;">
+      <div class="collapse collapse-horizontal" id="collapseWidthExample">
+        <div class="card card-body" style="width: 300px;">
+          This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
+        </div>
+      </div>
+    </div>
+    </li>
+    `;
+  });
+  return html;
+}
