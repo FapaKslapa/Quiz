0cb9f6724f2423634bb76f67e7466bf818558989
FapaKslapa <114689671+FapaKslapa@users.noreply.github.com>
2024-02-05 09:40:49 +0000

    Bug fix

--- public/script.js
+++ public/script.js
@@ -2,11 +2,23 @@ const modalQuiz = new bootstrap.Modal("#modalQuiz");
 const modalName = new bootstrap.Modal("#modalName");
 const inviaUser = document.getElementById("inviaUser");
 const username = document.getElementById("username");
+let tempo = 60; // Inserisci il tempo in secondi
+let countdown;
 inviaUser.onclick = () => {
   if (username.value) {
     modalName.hide();
     getQuestion().then((data) => {
       modalQuiz.show();
+      countdown = setInterval(() => {
+        document.getElementById("modalQuizTitle").innerText = tempo;
+        tempo--;
+
+        if (tempo < 0) {
+          clearInterval(countdown);
+          document.getElementById("modalQuizTitle").innerText =
+            "Tempo esaurito!";
+        }
+      }, 1000);
     });
   }
 };
@@ -21,15 +33,3 @@ const getQuestion = () => {
       });
   });
 };
-
-let tempo = 60; // Inserisci il tempo in secondi
-
-const countdown = setInterval(() => {
-  document.getElementById('modalQuizTitle').innerText = tempo;
-  tempo--;
-
-  if (tempo < 0) {
-    clearInterval(countdown);
-    document.getElementById('modalQuizTitle').innerText = 'Tempo esaurito!';
-  }
-}, 1000);
