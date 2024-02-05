83ecb719d8d5f2063795207d3f18173b1b5cb659
FapaKslapa <114689671+FapaKslapa@users.noreply.github.com>
2024-02-05 08:17:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+let todos = [];
+
+
+const server = http.createServer(app);
+server.listen(80, () => {
+  console.log("- server running");
+});
+
+app.get("/question", (req, res) => {
+  
+}) 
